ada3fed12173b6864f4702e92a99363bf2872ebd
Holes70 <holespato@gmail.com>
2025-01-20 21:41:38 +0100

    cargotml

--- src/window.rs
+++ src/window.rs
@@ -3,25 +3,25 @@ use opencv::{highgui, prelude::*};
 type Result<T> = opencv::Result<T>;
 
 pub(crate) struct Window {
-    name: String,
+  name: String,
 }
 
 impl Window {
-    pub fn create(name: &'_ str, width: i32, height: i32) -> Result<Self> {
-        highgui::named_window(name, highgui::WINDOW_GUI_NORMAL | highgui::WINDOW_KEEPRATIO)?;
-        highgui::resize_window(name, width, height)?;
-        Ok(Self {
-            name: name.to_owned(),
-        })
-    }
+  pub fn create(name: &'_ str, width: i32, height: i32) -> Result<Self> {
+    highgui::named_window(name, highgui::WINDOW_GUI_NORMAL | highgui::WINDOW_KEEPRATIO)?;
+    highgui::resize_window(name, width, height)?;
+    Ok(Self {
+      name: name.to_owned(),
+    })
+  }
 
-    pub fn show_image(&self, frame: &Mat) -> Result<()> {
-        highgui::imshow(&self.name, &frame)
-    }
+  pub fn show_image(&self, frame: &Mat) -> Result<()> {
+    highgui::imshow(&self.name, &frame)
+  }
 }
 
 impl Drop for Window {
-    fn drop(&mut self) {
-        let _ = highgui::destroy_window(&self.name);
-    }
+  fn drop(&mut self) {
+    let _ = highgui::destroy_window(&self.name);
+  }
 }
