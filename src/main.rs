3f7b5709fac290adbc089f8eb8fc1a57b253af3b
Holes70 <holespato@gmail.com>
2025-01-20 21:36:31 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod window;
 use crate::capture::Capture;
 use crate::window::Window;
 use opencv::core::{Rect, Scalar, Size, Vector};
-use opencv::{prelude::*, highgui, imgproc, objdetect};
+use opencv::{highgui, imgproc, objdetect, prelude::*};
 
 type Result<T> = opencv::Result<T>;
 
@@ -18,199 +18,193 @@ const SCALE_FACTOR: f64 = 0.25f64;
 const SCALE_FACTOR_INV: i32 = (1f64 / SCALE_FACTOR) as i32;
 
 fn try_run() -> Result<()> {
-  if !std::path::Path::new(CASCADE_XML_FILE).exists() {
-    panic!("Cascade file not found at: {}", CASCADE_XML_FILE);
-  }
+    if !std::path::Path::new(CASCADE_XML_FILE).exists() {
+        panic!("Cascade file not found at: {}", CASCADE_XML_FILE);
+    }
 
-  let mut classifier = objdetect::CascadeClassifier::new(CASCADE_XML_FILE)?;
+    let mut classifier = objdetect::CascadeClassifier::new(CASCADE_XML_FILE)?;
 
-  // Select capture width.
-  let capture_width = match get_capture_width() {
-    Ok(value) => value,
-    Err(error) => return Err(error),
-  };
+    // Select capture width.
+    let capture_width = match get_capture_width() {
+        Ok(value) => value,
+        Err(error) => return Err(error),
+    };
 
-  // Select capture height.
-  let capture_height = match get_capture_height() {
-    Ok(value) => value,
-    Err(error) => return Err(error),
-  };
+    // Select capture height.
+    let capture_height = match get_capture_height() {
+        Ok(value) => value,
+        Err(error) => return Err(error),
+    };
 
-  // Select capture device.
-  let mut capture = get_capture_device(capture_width, capture_height)?;
+    // Select capture device.
+    let mut capture = get_capture_device(capture_width, capture_height)?;
 
-  let opened = capture.is_opened()?;
-  if !opened {
-    panic!("Unable to open default camera!");
-  }
+    let opened = capture.is_opened()?;
+    if !opened {
+        panic!("Unable to open default camera!");
+    }
 
-  let window = Window::create(WINDOW_NAME, capture_width, capture_height)?;
+    let window = Window::create(WINDOW_NAME, capture_width, capture_height)?;
 
-  run_main_loop(&mut capture, &mut classifier, &window)?;
+    run_main_loop(&mut capture, &mut classifier, &window)?;
 
-  Ok(())
+    Ok(())
 }
 
-fn get_capture_device(
-  capture_width: i32,
-  capture_height: i32
-) -> Result<Capture> {
-  if let Ok(value) = std::env::var("CAPTURE_DEVICE") {
-    if let Ok(index) = value.parse() {
-      println!("Using capture device #{index}");
-      Capture::create(index, capture_width, capture_height)
+fn get_capture_device(capture_width: i32, capture_height: i32) -> Result<Capture> {
+    if let Ok(value) = std::env::var("CAPTURE_DEVICE") {
+        if let Ok(index) = value.parse() {
+            println!("Using capture device #{index}");
+            Capture::create(index, capture_width, capture_height)
+        } else {
+            let error = format!("Invalid camera selection: CAPTURE_DEVICE={value}");
+            Err(opencv::Error::new(1, error))
+        }
     } else {
-      let error = format!("Invalid camera selection: CAPTURE_DEVICE={value}");
-      Err(opencv::Error::new(1, error))
+        println!("Using default capture device");
+        Capture::create_default(capture_width, capture_height)
     }
-  } else {
-    println!("Using default capture device");
-    Capture::create_default(capture_width, capture_height)
-  }
 }
 
 fn get_capture_height() -> Result<i32> {
-  if let Ok(value) = std::env::var("CAPTURE_HEIGHT") {
-    if let Ok(height) = value.parse() {
-      println!("Using capture height {height}");
-      Ok(height)
+    if let Ok(value) = std::env::var("CAPTURE_HEIGHT") {
+        if let Ok(height) = value.parse() {
+            println!("Using capture height {height}");
+            Ok(height)
+        } else {
+            let error = format!("Invalid capture height: CAPTURE_HEIGHT={value}");
+            Err(opencv::Error::new(1, error))
+        }
     } else {
-      let error = format!("Invalid capture height: CAPTURE_HEIGHT={value}");
-      Err(opencv::Error::new(1, error))
+        println!("Using default capture height ({DEFAULT_CAPTURE_HEIGHT})");
+        Ok(DEFAULT_CAPTURE_HEIGHT)
     }
-  } else {
-    println!("Using default capture height ({DEFAULT_CAPTURE_HEIGHT})");
-    Ok(DEFAULT_CAPTURE_HEIGHT)
-  }
 }
 
 fn get_capture_width() -> Result<i32> {
-  if let Ok(value) = std::env::var("CAPTURE_WIDTH") {
-    if let Ok(width) = value.parse() {
-      println!("Using capture width {width}");
-      Ok(width)
+    if let Ok(value) = std::env::var("CAPTURE_WIDTH") {
+        if let Ok(width) = value.parse() {
+            println!("Using capture width {width}");
+            Ok(width)
+        } else {
+            let error = format!("Invalid capture width: CAPTURE_WIDTH={value}");
+            Err(opencv::Error::new(1, error))
+        }
     } else {
-      let error = format!("Invalid capture width: CAPTURE_WIDTH={value}");
-      Err(opencv::Error::new(1, error))
+        println!("Using default capture width ({DEFAULT_CAPTURE_WIDTH})");
+        Ok(DEFAULT_CAPTURE_WIDTH)
     }
-  } else {
-    println!("Using default capture width ({DEFAULT_CAPTURE_WIDTH})");
-    Ok(DEFAULT_CAPTURE_WIDTH)
-  }
 }
 
 fn run_main_loop(
-  capture: &mut Capture,
-  classifier: &mut objdetect::CascadeClassifier,
-  window: &Window,
+    capture: &mut Capture,
+    classifier: &mut objdetect::CascadeClassifier,
+    window: &Window,
 ) -> Result<()> {
-  loop {
-    const KEY_CODE_ESCAPE: i32 = 27;
-    if let Ok(KEY_CODE_ESCAPE) = highgui::wait_key(10) {
-      return Ok(());
+    loop {
+        const KEY_CODE_ESCAPE: i32 = 27;
+        if let Ok(KEY_CODE_ESCAPE) = highgui::wait_key(10) {
+            return Ok(());
+        }
+
+        let mut frame = match capture.grab_frame()? {
+            Some(frame) => frame,
+            None => continue,
+        };
+
+        let preprocessed = preprocess_image(&frame)?;
+        let faces = detect_faces(classifier, preprocessed)?;
+        for face in faces {
+            draw_box_around_face(&mut frame, face)?;
+        }
+
+        window.show_image(&frame)?;
     }
-
-    let mut frame = match capture.grab_frame()? {
-      Some(frame) => frame,
-      None => continue,
-    };
-
-    let preprocessed = preprocess_image(&frame)?;
-    let faces = detect_faces(classifier, preprocessed)?;
-    for face in faces {
-      draw_box_around_face(&mut frame, face)?;
-    }
-
-    window.show_image(&frame)?;
-  }
 }
 
 fn preprocess_image(frame: &Mat) -> Result<Mat> {
-  let gray = convert_to_grayscale(frame)?;
-  let reduced = reduce_image_size(&gray, SCALE_FACTOR)?;
-  equalize_image(&reduced)
+    let gray = convert_to_grayscale(frame)?;
+    let reduced = reduce_image_size(&gray, SCALE_FACTOR)?;
+    equalize_image(&reduced)
 }
 
 fn convert_to_grayscale(frame: &Mat) -> Result<Mat> {
-  let mut gray = Mat::default();
-  imgproc::cvt_color(frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
-  Ok(gray)
+    let mut gray = Mat::default();
+    imgproc::cvt_color(frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
+    Ok(gray)
 }
 
 fn reduce_image_size(gray: &Mat, factor: f64) -> Result<Mat> {
-  // Destination size is determined by scaling `factor`, not by target size.
-  const SIZE_AUTO: Size = Size {
-    width: 0,
-    height: 0,
-  };
-  let mut reduced = Mat::default();
-  imgproc::resize(
-    gray,
-    &mut reduced,
-    SIZE_AUTO,
-    factor, // fx
-    factor, // fy
-    imgproc::INTER_LINEAR,
-  )?;
-  Ok(reduced)
+    // Destination size is determined by scaling `factor`, not by target size.
+    const SIZE_AUTO: Size = Size {
+        width: 0,
+        height: 0,
+    };
+    let mut reduced = Mat::default();
+    imgproc::resize(
+        gray,
+        &mut reduced,
+        SIZE_AUTO,
+        factor, // fx
+        factor, // fy
+        imgproc::INTER_LINEAR,
+    )?;
+    Ok(reduced)
 }
 
 fn equalize_image(reduced: &Mat) -> Result<Mat> {
-  let mut equalized = Mat::default();
-  imgproc::equalize_hist(reduced, &mut equalized)?;
-  Ok(equalized)
+    let mut equalized = Mat::default();
+    imgproc::equalize_hist(reduced, &mut equalized)?;
+    Ok(equalized)
 }
 
-fn detect_faces(
-  classifier: &mut objdetect::CascadeClassifier,
-  image: Mat,
-) -> Result<Vector<Rect>> {
-  const SCALE_FACTOR: f64 = 1.1;
-  const MIN_NEIGHBORS: i32 = 2;
-  const FLAGS: i32 = 0;
-  const MIN_FACE_SIZE: Size = Size {
-    width: 30,
-    height: 30,
-  };
-  const MAX_FACE_SIZE: Size = Size {
-    width: 0,
-    height: 0,
-  };
+fn detect_faces(classifier: &mut objdetect::CascadeClassifier, image: Mat) -> Result<Vector<Rect>> {
+    const SCALE_FACTOR: f64 = 1.1;
+    const MIN_NEIGHBORS: i32 = 2;
+    const FLAGS: i32 = 0;
+    const MIN_FACE_SIZE: Size = Size {
+        width: 30,
+        height: 30,
+    };
+    const MAX_FACE_SIZE: Size = Size {
+        width: 0,
+        height: 0,
+    };
 
-  let mut faces = Vector::<Rect>::new();
+    let mut faces = Vector::<Rect>::new();
 
-  classifier.detect_multi_scale(
-    &image,
-    &mut faces,
-    SCALE_FACTOR,
-    MIN_NEIGHBORS,
-    FLAGS,
-    MIN_FACE_SIZE,
-    MAX_FACE_SIZE,
-  )?;
-  Ok(faces)
+    classifier.detect_multi_scale(
+        &image,
+        &mut faces,
+        SCALE_FACTOR,
+        MIN_NEIGHBORS,
+        FLAGS,
+        MIN_FACE_SIZE,
+        MAX_FACE_SIZE,
+    )?;
+    Ok(faces)
 }
 
 fn draw_box_around_face(frame: &mut Mat, face: Rect) -> Result<()> {
-  println!("found face {:?}", face);
-  let scaled_face = Rect {
-    x: face.x * SCALE_FACTOR_INV,
-    y: face.y * SCALE_FACTOR_INV,
-    width: face.width * SCALE_FACTOR_INV,
-    height: face.height * SCALE_FACTOR_INV,
-  };
+    println!("found face {:?}", face);
+    let scaled_face = Rect {
+        x: face.x * SCALE_FACTOR_INV,
+        y: face.y * SCALE_FACTOR_INV,
+        width: face.width * SCALE_FACTOR_INV,
+        height: face.height * SCALE_FACTOR_INV,
+    };
 
-  const THICKNESS: i32 = 2;
-  const LINE_TYPE: i32 = 8;
-  const SHIFT: i32 = 0;
-  let color_red = Scalar::new(0f64, 0f64, 255f64, -1f64);
+    const THICKNESS: i32 = 2;
+    const LINE_TYPE: i32 = 8;
+    const SHIFT: i32 = 0;
+    let color_red = Scalar::new(0f64, 0f64, 255f64, -1f64);
 
-  imgproc::rectangle(frame, scaled_face, color_red, THICKNESS, LINE_TYPE, SHIFT)?;
-  Ok(())
+    imgproc::rectangle(frame, scaled_face, color_red, THICKNESS, LINE_TYPE, SHIFT)?;
+    Ok(())
 }
 
 fn main() -> Result<()> {
-  try_run()?;
+    try_run()?;
 
-  Ok(())
+    Ok(())
 }
